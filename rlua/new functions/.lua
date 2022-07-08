e70b305afe22add71f7a8fded0ee8af1f300e815
complex <107022425+ComplexGithub@users.noreply.github.com>
2022-07-08 05:27:15 -0500

    Update and rename rlua/f.lua to rlua/new functions/.lua

---
+++ rlua/new functions/.lua
@@ -0,0 +1,58 @@
+-- Remade/new functions for scripts
+-- complex#0002 (900187302016471092)
+
+local F = {}
+
+F.HTTPEncode = function(Data)
+    return game:GetService("HttpService"):JSONEncode(Data)
+end
+
+F.HTTPDecode = function(Data)
+    return game:GetService("HttpService"):JSONDecode(Data)
+end
+
+F.GetPlayers = function()
+    return game:GetService("Players"):GetPlayers()
+end
+
+F.FromHex = function(Integer)
+    if type(Integer) == "string" then
+        if Integer:find("0x") then
+            Integer = tonumber(Integer)
+
+            R = ((Integer / (256 ^ 2)) % 256) / 255
+            G = ((Integer / 256) % 256) / 255
+            B = (Integer % 256) / 255
+
+            return Color3.new(R, G, B)
+        elseif Integer:find("#") then
+            Integer = tonumber("0x" .. Integer:gsub("#", ""))
+
+            R = ((Integer / (256 ^ 2)) % 256) / 255
+            G = ((Integer / 256) % 256) / 255
+            B = (Integer % 256) / 255
+
+            return Color3.new(R, G, B)
+        else
+            Integer = tonumber("0x" .. Integer)
+
+            R = ((Integer / (256 ^ 2)) % 256) / 255
+            G = ((Integer / 256) % 256) / 255
+            B = (Integer % 256) / 255
+
+            return Color3.new(R, G, B)
+        end
+    elseif type(Integer) == "number" then
+        Integer = tonumber(Integer)
+
+        R = ((Integer / (256 ^ 2)) % 256) / 255
+        G = ((Integer / 256) % 256) / 255
+        B = (Integer % 256) / 255
+
+        return Color3.new(R, G, B)
+    end
+end
+
+F.TableRemove = function(Table, Value)
+    table.remove(Table, table.find(Table, Value))
+end
