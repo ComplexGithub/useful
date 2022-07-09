e9bf1aa92b3069d274a5ea41998750ef705747f7
complex <107022425+ComplexGithub@users.noreply.github.com>
2022-07-09 15:20:17 -0500

    Update .lua

--- rlua/syn-console/.lua
+++ rlua/syn-console/.lua
@@ -1,1 +1,26 @@
+-- Synapse X Console functions
+-- complex#0002 (900187302016471092)
 
+function rconsolestructure(MessageColor, Type, TypeColor)
+    rconsoleprint("@@" .. MessageColor:gsub(" ", "_"):upper() .. "@@")
+    rconsoleprint("[")
+    rconsoleprint("@@" .. TypeColor:gsub(" ", "_"):upper() .. "@@")
+    rconsoleprint(Type:upper())
+    rconsoleprint("@@" .. MessageColor:gsub(" ", "_"):upper() .. "@@")
+    rconsoleprint("]: ")
+end
+
+getgenv().print = function(Message)
+    rconsolestructure("white", "info", "light blue")
+    rconsoleprint(Message .. "\n")
+end
+
+getgenv().warn = function(Message)
+    rconsolestructure("white", "warn", "yellow")
+    rconsoleprint(Message .. "\n")
+end
+
+getgenv().error = function(Message)
+    rconsolestructure("white", "Error", "light red")
+    rconsoleprint(Message .. "\n")
+end
