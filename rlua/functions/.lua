9ecb947c88c6f877c1d4e9aa8f0a99296965d0ef
complex <107022425+ComplexGithub@users.noreply.github.com>
2022-07-18 23:54:55 -0500

    Update .lua

--- rlua/functions/.lua
+++ rlua/functions/.lua
@@ -15,7 +15,35 @@ function F:GetPlayers()
     return game:GetService("Players"):GetPlayers()
 end
 
-function F.C3Hex(Color)
+function F.TableDelete(Table, Value)
+    assert(type(Table) == "table", "Invalid argument #1 (table expected, got " .. type(Table) .. ")")
+    table.remove(Table, table.find(Table, Value))
+end
+
+function F:Tween(Object, TweenInfoData, PropertyData)
+    assert(type(Object) == "userdata", "Invalid argument #1 (userdata expected, got " .. type(Object) .. ")")
+    assert(type(TweenInfoData) == "userdata", "Invalid argument #2 (table expected, got " .. type(TweenInfoData) .. ")")
+    assert(type(PropertyData) == "userdata", "Invalid argument #3 (table expected, got " .. type(PropertyData) .. ")")
+    
+    local TI = TweenInfo.new(unpack(TweenInfoData))
+    local Tween = game:GetService("TweenService"):Create(Object, TI, PropertyData)
+    Tween:Play()
+end
+
+function F:Create(InstanceName, Properties)
+    assert(type(InstanceName) == "string", "Invalid argument #1 (string expected, got " .. type(InstanceName) .. ")")
+    assert(type(Properties) == "table", "Invalid argument #2 (table expected, got " .. type(Properties) .. ")")
+    
+    local CreatedInstance = Instance.new(InstanceName)
+    
+    for Property, Value in pairs(Properties) do
+        CreatedInstance[Property] = Value
+    end
+    
+    return CreatedInstance
+end
+
+function F:Color3Hex(Color)
     assert((type(Color) == "string") or (type(Color) == "number"), "Invalid argument #1 (string/number expected, got " .. type(Color) .. ")")
 
     if type(Color) == "string" then
@@ -37,7 +65,7 @@ function F.C3Hex(Color)
     end
 end
 
-function F.C3Inverse(Color)
+function F:Color3Inverse(Color)
     assert(type(Color) == "userdata", "Invalid argument #1 (userdata expected, got " .. type(Color) .. ")")
 
     R = 1 - Color.R
@@ -45,35 +73,6 @@ function F.C3Inverse(Color)
     B = 1 - Color.B
 
     return Color3.new(R, G, B)
-end
-
-function F.TableDelete(Table, Value)
-    assert(type(Table) == "table", "Invalid argument #1 (table expected, got " .. type(Table) .. ")")
-    table.remove(Table, table.find(Table, Value))
-end
-
-function F:Tween(Object, TweenInfoData, PropertyData)
-    assert(type(Object) == "userdata", "Invalid argument #1 (userdata expected, got " .. type(Object) .. ")")
-    assert(type(TweenInfoData) == "userdata", "Invalid argument #2 (table expected, got " .. type(TweenInfoData) .. ")")
-    assert(type(PropertyData) == "userdata", "Invalid argument #3 (table expected, got " .. type(PropertyData) .. ")")
-    
-    local TI = TweenInfo.new(unpack(TweenInfoData))
-    local Tween = game:GetService("TweenService"):Create(Object, TI, PropertyData)
-    Tween:Play()
-end
-
-function F:Create(Instance, Properties)
-    assert(type(Instance) == "string", "Invalid argument #1 (string expected, got " .. type(Instance) .. ")")
-    assert(type(Properties) == "table", "Invalid argument #2/3 (table expected, got " .. type(Properties) .. ")")
-    
-    local CreatedInstance = Instance.new(Instance)
-    
-    for Property, Value in pairs(Properties) do
-        assert(CreatedInstance[Property], Property .. " is not a valid property of the created Instance")
-        CreatedInstance[Property] = Value
-    end
-    
-    return CreatedInstance
 end
 
 return F
